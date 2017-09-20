1cc4191d860a7b1574d6052e24e52899c0d0d68c
Tomasz Strojny <tomasz@init.biz>
2017-09-20 19:08:44 +0200

    Repaired signInToBackend method

--- traits/OctoberSeleniumHelpers.php
+++ traits/OctoberSeleniumHelpers.php
@@ -8,9 +8,11 @@ trait OctoberSeleniumHelpers
 {
     protected function signInToBackend()
     {
-        return $this->visit(TEST_SELENIUM_BACKEND_URL)
+        $this->visit(TEST_SELENIUM_BACKEND_URL)
              ->type(TEST_SELENIUM_USER, 'login')
              ->type(TEST_SELENIUM_PASS, 'password')
-             ->click("//button[@type='submit']");
+             ->findElement("Login button", "//button[@type='submit']")
+             ->click();
+        return $this;
     }
 }
